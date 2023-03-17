7bbc436a2c35c986114b1810652681aa1d9cca5e
cemtaskin <cemtaskin82@gmail.com>
2023-03-17 21:02:56 +0300

    contructor example

--- Main.java
+++ Main.java
@@ -3,27 +3,16 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-       
-        Student student = new Student(); //it creates a new object from Student class - > create an instance of Student class
-        student.name="Ali";
-        student.surname="Duru";
-        student.number="1001";
-        student.dateOfBirth=LocalDate.of(1985, 1, 1);
 
-            
-       
-        Student student2 = new Student();
-        student2.name="Aliye";
-        student2.surname="Duru";
-        student2.number="1002";
-        student2.dateOfBirth=LocalDate.of(1982,1,19);
-        
+        Student student = new Student("Ali", "Duru", "1001", LocalDate.of(1985, 1, 1));
+        Student student2 = new Student("Aliye", "Duru", "1002", LocalDate.of(1982, 1, 19));
+
         student.sayHello();
         student.sayNumber();
         student.sayNumber(5);
 
         student2.sayHello();
-        
+
         student.sayAge();
         student2.sayAge();
     }
--- Student.java
+++ Student.java
@@ -1,8 +1,6 @@
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.Period;
-import java.time.ZoneId;
-import java.util.Date;
+
 
 public class Student{
 
@@ -10,7 +8,20 @@ public class Student{
     String surname;
     String number;
     LocalDate dateOfBirth;
+
+     
+    public Student(){ //this is constructor method with no params. Zero paramter constructor method is called as default constructor
+        System.out.println("Def constructor is triggered automatically");
+    }
     
+    public Student(String name, String surname, String number, LocalDate dateOfBirth) {
+        this.name = name;
+        this.surname = surname;
+        this.number = number;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+
     //a method with no paramaters  named as sayHello. 
     void sayHello (){
         System.out.println(String.format("Hi my name is %s. My Surname is %s and my number is %s",name,surname,number));
